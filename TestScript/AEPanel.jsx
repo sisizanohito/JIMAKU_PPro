e37fde6347127924f45a2ebfa0cbed8a9041a869
sisizanohito <sisizanohito@gmail.com>
2019-01-23 23:13:46 +0900

    psd読み込みを分離

---
+++ TestScript/AEPanel.jsx
@@ -0,0 +1,18 @@
+﻿var wnd= (this instanceof Panel) ? this : new Window('palette', 'Auto Slide Show', undefined, {resizeable: true});
+var okBtn = wnd.add("button", [55, 100, 140, 135], "OK");
+var cancelBtn = wnd.add("button", [155, 100, 240, 135], "Cancel");
+
+if (winObj instanceof Window) {  
+wnd.center();  
+wnd.show();  
+} else {  
+wnd.layout.layout(true);  
+wnd.layout.resize();  
+}  
+
+okBtn.onClick = function(){
+    alert("OK !");
+}
+cancelBtn.onClick = function(){
+    wnd.close();
+}
